fad351ee22e8c130004093f3e42c741fa3314c17
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-05-18 23:40:41 +0300

    (5 Copy Files From Source To Destination )

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,12 @@ var gulp = require('gulp');
 //Your First Task
 
 gulp.task('moh', function() {
-    console.log('mmmmmmmmmmmmmm')
+    // return gulp.src('project/index.html')
+    // return gulp.src('project/*.html')
+    // return gulp.src('project/*.*')
+    return gulp.src(['project/index.html', 'project/main.css'])
+
+    // .pipe(gulp.dest('/dist'))
+    .pipe(gulp.dest('/dist/copy'))
+
 })
