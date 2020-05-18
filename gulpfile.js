9aac39cdaceaa78a87ff862d0c473f9eefc20a15
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-05-18 23:17:17 +0300

    (4 ترتيب التاسك)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
 
-
 //Your First Task
 
-gulp.task('MohAzzam', function() {
-    console.log('Gratz For Your First Task');
-});
+gulp.task('moh', function() {
+    console.log('mmmmmmmmmmmmmm')
+})
