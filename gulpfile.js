3d43195c9acaad26755c5b4955d9c988b7f35579
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-05-19 16:14:37 +0300

    (9 How To Compile Pugjs Files With Gulp Task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     prefix = require('gulp-autoprefixer'),
-    sass = require('gulp-sass')
- 
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug');
+
+
+//Html Task
+gulp.task('html', function() {
+    return gulp.src('project/index.pug')
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest('dist'))
+})
 
 
 //Css Task
 gulp.task('css', function() {
 
     return gulp.src('project/css/main.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(prefix())
         // .pipe(prefix('last 2 versions'))
         .pipe(concat('test.css'))
